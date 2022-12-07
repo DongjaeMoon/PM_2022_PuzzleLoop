7aad2adeb52304cc9fbb4812b650ce659700c077
djmoon <djmoon2002@snu.ac.kr>
2022-12-07 15:42:36 +0900

    Archive: ./PM_project/Constants.h

---
+++ PM_project/Constants.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#define WINDOW_X 200
+#define WINDOW_Y 200
+
+#define WINDOW_WIDTH 640		// window's width
+#define WINDOW_HEIGHT 640		// window's height
+
+#define boundaryX (WINDOW_WIDTH)/2
+#define boundaryY (WINDOW_HEIGHT)/2
+
+#define gameLeftBoundary -320
+#define gameRightBoundary 320
+#define gameUpperBoundary 320
+#define gameLowerBoundary -320
+
+#define sphereRadius 20.0f
+#define gameSpeed 7.0f // game is slower with big gamespeed
+#define PI 3.14159265f
